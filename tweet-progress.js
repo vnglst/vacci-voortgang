636490c70a548c4434cd0ab00a0db471cc765290
Koen van Gilst <koen@koenvangilst.nl>
2021-05-22 21:40:11 +0200

    feature: add fully vaccinated

--- tweet-progress.js
+++ tweet-progress.js
@@ -4,6 +4,9 @@ const Twitter = require("twitter");
 const puppeteer = require("puppeteer");
 const env = require("env-var");
 
+const POPULATION = 17_480_481;
+const TOTAL_BLOCKS = 10;
+
 const CONSUMER_KEY = env.get("CONSUMER_KEY").required().asString();
 const CONSUMER_SECRET = env.get("CONSUMER_SECRET").required().asString();
 const ACCESS_TOKEN = env.get("ACCESS_TOKEN").required().asString();
@@ -22,31 +25,33 @@ const client = new Twitter({
 async function scrape() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
   await page.goto(
     "https://www.rivm.nl/covid-19-vaccinatie/cijfers-vaccinatieprogramma"
   );
-  const value = await page.evaluate(() => {
+
+  const result = await page.evaluate(() => {
     let tds = document.querySelectorAll(
       "[class='table table-striped-brand-lightest'] tr:last-child td"
     );
-    return tds[3].textContent;
+    return {
+      atLeastOne: tds[3].textContent.replace(/\./g, ""),
+      fully: tds[4].textContent.replace(/\./g, ""),
+    };
   });
+
   browser.close();
-  return value.replace(/\./g, "");
+  return result;
 }
 
 async function main() {
-  const POPULATION = 17_480_481;
-  const TOTAL_BLOCKS = 10;
-  const vaccinated = await scrape();
-  const percentage = (vaccinated / POPULATION) * 100;
-  const blocksDone = Math.round((percentage / 100) * TOTAL_BLOCKS);
+  const { atLeastOne, fully } = await scrape();
 
-  // can't use padStart/End with unicode characters, using this work around
-  const progress = "".padStart(blocksDone, "X").padEnd(TOTAL_BLOCKS, "O");
-  const niceProgress = progress.replaceAll("X", "🟩").replaceAll("O", "⬜️");
-
-  const message = `${niceProgress} ${percentage.toFixed(2).replace(".", ",")}%`;
+  const message =
+    "Eén prik:\n" +
+    getProgressStr(atLeastOne) +
+    "\n\nVolledig gevaccineerd:\n" +
+    getProgressStr(fully);
 
   client.post(
     "statuses/update",
@@ -59,4 +64,15 @@ async function main() {
   return `${message}`;
 }
 
-main().then(console.log).catch(console.error);
+function getProgressStr(vaccinatedPersons) {
+  const percentage = (vaccinatedPersons / POPULATION) * 100;
+  const blocksDone = Math.round((percentage / 100) * TOTAL_BLOCKS);
+
+  // can't use padStart/End with unicode characters, using this work around
+  const progress = "".padStart(blocksDone, "X").padEnd(TOTAL_BLOCKS, "O");
+  const niceProgress = progress.replaceAll("X", "🟩").replaceAll("O", "⬜️");
+
+  return `${niceProgress} ${percentage.toFixed(0)}%`;
+}
+
+main().then(console.log);
